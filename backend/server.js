e6654b83800dd06d087118a9d71afc51423e70aa
Aaditya Ashwin Rao <aadityaashwinrao@gmail.com>
2026-08-11 11:53:50 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import app from "./src/app.js";
 import connectDB from "./src/utils/connectionDb.js";
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
+app.listen(PORT,"0.0.0.0",()=>{
     console.log(`Backend is running in PORT ${PORT}`)
 })
 
